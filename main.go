e7c5047445f52f389c4b3052d4145e34e137c8b4
amy324 <144127459+amy324@users.noreply.github.com>
2024-01-20 19:38:33 +0000

    update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 
@@ -28,20 +29,73 @@ func (p *Producer) Close() error {
 	return <- ch
 }
 
-func pizzeria(pizzaMaker *Producer) {
-	//keep track of whoch pizza is being made
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumberOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received order #%d\n", pizzaNumber)
 
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza#%d. It will take %d seconds...\n", pizzaNumber, delay)
+		// Delay
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** Order cannot be fulfilled as ingredients are out of stock for pizza #%d!", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** Order cannot be fulfilled due to staffing issues for pizza #%d!", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("Pizza order #%d is ready!", pizzaNumber)
+		}
+
+		p := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+
+		return &p
+	}
+
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
+
+func pizzeria(pizzaMaker *Producer) {
+	//keep track of which pizza is being made
+	var i = 0
 	//continuously run until we recieve quit notification
 
 	//try to make pizzas
 	for {
-
-		//try to mmake a pizza
-
-		//decision
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+			select {
+				//data sent to data channel which symbolises that an attempt to make a pizza was made
+			case pizzaMaker.data <- *currentPizza:
+			case quitChan := <- pizzaMaker.quit:
+				//close channels 
+				close(pizzaMaker.data)
+				close(quitChan)
+				//return exits go return
+				return
+			}
+		}
 	}
-
-
 }
 
 func main() {
